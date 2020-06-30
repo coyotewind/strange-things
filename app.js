a92015369184950c3f12de9227a877f7b7d5c7ef
John Marcello <john.marcello@yahoo.com>
2020-06-30 13:17:22 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+$(".action").on("click", function() {
+    var modal = $(this).data("modal");
+    $(modal).show();
+  });
+  
+  $(".modal").on("click", function(e) {
+    var className = e.target.className;
+    if(className === "modal" || className === "close"){
+      $(this).closest(".modal").hide();
+    }
+  });
